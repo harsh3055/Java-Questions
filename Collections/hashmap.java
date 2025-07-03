7a5933d25cef18b5992c930b373cce4fddc5fc24
Harsh Goswami <harsh.goswami3054@gmail.com>
2025-07-04 00:26:18 +0530

    hashmap

---
+++ Collections/hashmap.java
@@ -0,0 +1,19 @@
+package org.example.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class hashmap {
+
+    public static void main(String[] args) {
+
+        // Create a Map using HashMap
+        Map<String, Integer> m = new HashMap<>();
+
+        // Adding key-value pairs to the map
+        m.put("Geek1", 1);
+        m.put("Geek2", 2);
+        m.put("Geek3", 3);
+
+        System.out.println("Map elements: " + m);
+    }
+}
